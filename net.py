0e188c1df724374a944fe9e854df20d4c19d002f
Jamie <j-github@sdcmail.com.au>
2022-10-27 11:27:11 +1100

    First working version

--- net.py
+++ net.py
@@ -12,23 +12,24 @@ class convNN(torch.nn.Module):
         # TODO: These values are all placeholders. To be filled when data shape is known
         # Pooling and convolutional layers
         # TODO: Change some settings for colour images
-        self.conv1 = nn.conv2d(1, 1)
-        self.conv2 = nn.conv2d(1, 1)
-        self.pool = nn.MaxPool2d(5)
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.sig = nn.Sigmoid()
 
         # Fully connected layers
-        self.fc1 = nn.Linear(10, 6)
-        self.fc2 = nn.Linear(6, 4)
-        self.fc3 = nn.Linear(4, 1)
+        self.fc1 = nn.Linear(35344, 300)
+        self.fc2 = nn.Linear(300, 84)
+        self.fc3 = nn.Linear(84, 2)
 
     def forward(self, input):
-        x = self.pool(nn.sigmoid(self.conv1(input)))
-        x = self.pool(nn.sigmoid(self.conv2(x)))
+        x = self.pool(self.sig(self.conv1(input)))
+        x = self.pool(self.sig(self.conv2(x)))
 
-        x.flatten(x, 1)
+        x = torch.flatten(x)
 
-        x = nn.sigmoid(self.fc1(x))
-        x = nn.sigmoid(self.fc2(x))
-        x = nn.sigmoid(self.fc3(x))
+        x = self.sig(self.fc1(x))
+        x = self.sig(self.fc2(x))
+        x = self.sig(self.fc3(x))
 
         return x
