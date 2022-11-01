5b9bf9df315b3ef07cbf9f635750633a17589ba8
Jamie <j-github@sdcmail.com.au>
2022-11-01 22:39:06 +1100

    Option to train using CUDA

--- train.py
+++ train.py
@@ -39,10 +39,7 @@ def print_percent_done(index, total, bar_len=50, title='Please wait'):
     if round(percent_done) == 100:
         print('\t✅')
 
-import random
-
 torch.manual_seed(42)
-torch.use_deterministic_algorithms(True)
 
 def evaluate(model):
     model.eval()
@@ -52,7 +49,7 @@ def evaluate(model):
     model.train()
     return 0
 
-def train(model, train_loader, lr, momentum=0.9, epochs=5):
+def train(model, train_loader, lr, device, momentum=0.9, epochs=5):
     loss_func = nn.MSELoss()
     optimizer = optim.SGD(model.parameters(), lr=lr)#, momentum=momentum)
 
@@ -61,9 +58,10 @@ def train(model, train_loader, lr, momentum=0.9, epochs=5):
             images, _, _, _, landmarks = data   # images, age, gender, race, landmarks
             # Zero paramter gradients
             optimizer.zero_grad()
+            images, landmarks = images.to(device), landmarks.to(device)
 
             outputs = model(images)
-            loss = loss_func(outputs, landmarks[:, 31].float())
+            loss = loss_func(outputs, landmarks[:, 31])
             loss.backward()
             optimizer.step()
 
@@ -94,35 +92,37 @@ if __name__ == "__main__":
                         metavar="MODEL_NAME")
     parser.add_argument("-lr", "--learning_rate",
                         help="Learning rate to run the optimizer function with.",
-                        default=0.001,
+                        default=0.0001,
+                        type=float,
+                        metavar="FLOAT")
+    parser.add_argument("--cuda",
+                        help="Add this argument to run the code using GPU acceleration.",
+                        action="store_true")
+    parser.add_argument("-e", "--epochs",
+                        help="Dictate number of epochs to train for.",
                         type=int,
-                        metavar="INT")
+                        metavar="INT",
+                        default=5)
 
     args = parser.parse_args()
 
+    device = "cpu"
+    if args.cuda and torch.cuda.is_available():
+        device = "cuda"
+
     model = None
     if args.model == "convNN2":
-        model = convNN2()
+        model = convNN2().to(device)
 
     UTKFace = CustomImageDataset(args.file, 'UTKFace')
     train_dataloader = DataLoader(UTKFace, batch_size=args.batch, shuffle=True)
 
     print(f"Training {args.model} from {args.file} with batch_size={args.batch}")
 
-    def seed_worker(worker_id):
-        worker_seed = torch.initial_seed() % 2**32
-        np.random.seed(worker_seed)
-        random.seed(worker_seed)
-
-    g = torch.Generator()
-    g.manual_seed(42)
-
     UTKFace = CustomImageDataset('landmark_list.txt', 'UTKFace')
     train_dataloader = DataLoader(UTKFace, 
                                     batch_size=64, 
-                                    shuffle=True, 
-                                    worker_init_fn=seed_worker,
-                                    generator=g,)
+                                    shuffle=True)
     #console = curses.initscr()
 
-    model = train(model, train_dataloader, args.learning_rate)
+    model = train(model, train_dataloader, args.learning_rate, device, epochs=args.epochs)
